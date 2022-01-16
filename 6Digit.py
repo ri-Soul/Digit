1d40c124632170d92c0a7ffa076e4b97330710ac
_StupidYou <91799709+Stupid4You@users.noreply.github.com>
2022-01-16 13:28:34 +0100

    Add files via upload

---
+++ 6Digit.py
@@ -0,0 +1,36 @@
+num = 0
+done = 0
+num0 = "00000"
+print("________  .__       .__  __   ")
+print("\______ \ |__| ____ |__|/  |_ ")
+print(" |    |  \|  |/ ___\|  \   __/")
+print(" |    `   \  / /_/  >  ||  |  ")
+print("/_______  /__\___  /|__||__|  ")
+print("        \/  /_____/           ")
+
+print("")
+print("Loading...")
+print("")
+
+while done == 0:
+  
+  if len(str(num)) == 2:
+    num0 = "0000"
+  if len(str(num)) == 3:
+    num0 = "000"
+  if len(str(num)) == 4:
+    num0 = "00"
+  if len(str(num)) == 5:
+    num0 = "0"
+  if len(str(num)) == 6:
+    num0 = ""
+  if num >= 999999:
+    done = 1
+	 
+  
+  with open('6Digits.txt', 'a') as f:
+    f.write("\n" + num0 +str(num))
+
+  num += 1
+  
+print("Done!")
